8be0747b16a78161f9d27977f4aa228d50eff40c
Alexandre CASTELLANE <alexandre.castellane@fr.ibm.com>
2020-11-24 19:44:36 +0100

    adding simul eth loopback wires
    Signed-off-by: Alexandre CASTELLANE <alexandre.castellane@fr.ibm.com>

--- sim/src/top.sv
+++ sim/src/top.sv
@@ -613,7 +613,20 @@ module top (
     wire   [4:0] f1_octrl00_pasid_len_supported;
     wire         f1_octrl00_metadata_supported;
     wire  [11:0] f1_octrl00_actag_len_supported;
-    
+  
+    // we define ethernet wires only if in simulation and emac requested (no loopback before emac)
+    `ifdef ENABLE_ETHERNET
+    `ifndef ENABLE_ETH_LOOP_BACK
+    wire  gt_trx_gt_port_0_p;
+    wire  gt_trx_gt_port_1_n;
+    wire  gt_trx_gt_port_1_p;
+    wire  gt_trx_gt_port_2_n;
+    wire  gt_trx_gt_port_2_p;
+    wire  gt_trx_gt_port_3_n;
+    wire  gt_trx_gt_port_3_p;
+    `endif
+    `endif
+ 
     `ifdef ENABLE_DDR
         wire          c0_ddr4_act_n;
         wire  [16:0]  c0_ddr4_adr;
@@ -1152,6 +1165,33 @@ module top (
 
         .afu_tlx_resp_credit                ( fen_afu_tlx_resp_credit                       ),
         .afu_tlx_resp_initial_credit        ( fen_afu_tlx_resp_initial_credit[6:0]          ),
+
+`ifdef ENABLE_ETHERNET
+`ifndef ENABLE_ETH_LOOP_BACK
+    .gt_ref_clk_n      ( ~sys_clk_p      )
+   ,.gt_ref_clk_p      ( sys_clk_p       )
+   ,.gt_rx_gt_port_0_n ( gt_trx_gt_port_0_n  )
+   ,.gt_rx_gt_port_0_p ( gt_trx_gt_port_0_p  )
+   ,.gt_rx_gt_port_1_n ( gt_trx_gt_port_1_n  )
+   ,.gt_rx_gt_port_1_p ( gt_trx_gt_port_1_p  )
+   ,.gt_rx_gt_port_2_n ( gt_trx_gt_port_2_n  )
+   ,.gt_rx_gt_port_2_p ( gt_trx_gt_port_2_p  )
+   ,.gt_rx_gt_port_3_n ( gt_trx_gt_port_3_n  )
+   ,.gt_rx_gt_port_3_p ( gt_trx_gt_port_3_p  )
+   ,.gt_tx_gt_port_0_n ( gt_trx_gt_port_0_n  )
+   ,.gt_tx_gt_port_0_p ( gt_trx_gt_port_0_p  )
+   ,.gt_tx_gt_port_1_n ( gt_trx_gt_port_1_n  )
+   ,.gt_tx_gt_port_1_p ( gt_trx_gt_port_1_p  )
+   ,.gt_tx_gt_port_2_n ( gt_trx_gt_port_2_n  )
+   ,.gt_tx_gt_port_2_p ( gt_trx_gt_port_2_p  )
+   ,.gt_tx_gt_port_3_n ( gt_trx_gt_port_3_n  )
+   ,.gt_tx_gt_port_3_p ( gt_trx_gt_port_3_p  ),
+`endif
+`endif
+
+
+
+
 
         `ifdef ENABLE_DDR
           `ifdef AD9V3
