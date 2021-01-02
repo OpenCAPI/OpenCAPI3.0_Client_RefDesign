83e95f20717d11376fb78b3ca3c42448fe924ee4
Filip Leonarski <filip.leonarski@psi.ch>
2021-01-02 14:45:02 +0000

    Added LED and EEPROM I/O interfaces for 9H3
    Signed-off-by: Filip Leonarski <filip.leonarski@psi.ch>

--- board_support_packages/ad9h335/verilog/framework_top/oc_fpga_top.v
+++ board_support_packages/ad9h335/verilog/framework_top/oc_fpga_top.v
@@ -91,6 +91,18 @@ module oc_fpga_top (
 `endif
 `endif
 
+`ifdef ENABLE_9H3_LED
+    , output                 user_led_a0
+    , output                 user_led_a1
+    , output                 user_led_g0
+    , output                 user_led_g1
+`endif
+`ifdef ENABLE_9H3_EEPROM
+    , inout                  eeprom_scl
+    , inout                  eeprom_sda
+    , inout                  eeprom_wp
+`endif 
+
 `ifdef FLASH
    ,inout  FPGA_FLASH_CE2_L       // To/From FLASH of flash_sub_system.v
    ,inout  FPGA_FLASH_DQ4         // To/From FLASH of flash_sub_system.v
@@ -1106,6 +1118,18 @@ oc_function oc_func(
    ,.gt_tx_gt_port_3_p ( gt_tx_gt_port_3_p  )
 `endif
 `endif
+
+`ifdef ENABLE_9H3_LED
+    , .user_led_a0     ( user_led_a0        )
+    , .user_led_a1     ( user_led_a1        )
+    , .user_led_g0     ( user_led_g0        )
+    , .user_led_g1     ( user_led_g1        )
+`endif
+`ifdef ENABLE_9H3_EEPROM
+    , .eeprom_scl      (eeprom_scl          )
+    , .eeprom_sda      (eeprom_sda          )
+    , .eeprom_wp       (eeprom_wp           )
+`endif 
 
 );
 
---
+++ board_support_packages/ad9h335/xdc/extra_9h3_interfaces.xdc
@@ -0,0 +1,8 @@
+set_property PACKAGE_PIN BE30 [get_ports {eeprom_scl}]    ; #SPARE_SCL
+set_property PACKAGE_PIN BC30 [get_ports {eeprom_sda}]    ; #SPARE_SDA
+set_property PACKAGE_PIN BD30 [get_ports {eeprom_wp}]     ; #SPARE_WP
+
+set_property PACKAGE_PIN AV32 [get_ports {user_led_a0}]   ; #USER_LED_A0_1V8
+set_property PACKAGE_PIN AW32 [get_ports {user_led_a1}]   ; #USER_LED_A1_1V8
+set_property PACKAGE_PIN AY30 [get_ports {user_led_g0}]   ; #USER_LED_G0_1V8
+set_property PACKAGE_PIN AV31 [get_ports {user_led_g1}]   ; #USER_LED_G1_1V8
