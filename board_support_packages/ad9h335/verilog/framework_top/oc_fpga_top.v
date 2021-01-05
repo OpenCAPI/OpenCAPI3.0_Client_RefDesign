228495ab70e316015b227a068906466691705b68
Filip Leonarski <filip.leonarski@psi.ch>
2021-01-05 13:16:51 +0000

    Added AVR management interface pins for Alpha Data 9H3 board
    Signed-off-by: Filip Leonarski <filip.leonarski@psi.ch>

--- board_support_packages/ad9h335/verilog/framework_top/oc_fpga_top.v
+++ board_support_packages/ad9h335/verilog/framework_top/oc_fpga_top.v
@@ -102,7 +102,11 @@ module oc_fpga_top (
     , inout                  eeprom_sda
     , output                 eeprom_wp
 `endif 
-
+`ifdef ENABLE_9H3_AVR
+    , input                  avr_rx
+    , output                 avr_tx
+    , input                  avr_ck
+`endif
 `ifdef FLASH
    ,inout  FPGA_FLASH_CE2_L       // To/From FLASH of flash_sub_system.v
    ,inout  FPGA_FLASH_DQ4         // To/From FLASH of flash_sub_system.v
@@ -1130,7 +1134,11 @@ oc_function oc_func(
     , .eeprom_sda      (eeprom_sda          )
     , .eeprom_wp       (eeprom_wp           )
 `endif 
-
+`ifdef ENABLE_9H3_AVR
+    , .avr_rx          (avr_rx              )
+    , .avr_tx          (avr_tx              )
+    , .avr_ck          (avr_ck              )
+`endif
 );
 
 
